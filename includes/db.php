85cb41e525074c67035d44bcd094f29b3903512e
Alfian57 <alfiangadingsaputra@gmail.com>
2025-01-02 19:25:17 +0700

    ci: add ci pipeline

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,20 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'gym_sistem';
+if (file_exists('.env')) {
+    $env = parse_ini_file('.env');
+    $dbHost = $env["DB_HOST"];
+    $dbUsername = $env["DB_USERNAME"];
+    $dbPassword = $env["DB_PASSWORD"];
+    $dbName = $env["DB_NAME"];
+} else {
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    $dbName = getenv("DB_NAME");
+}
 
-$conn = mysqli_connect($host, $user, $password, $database);
+
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
-?>
